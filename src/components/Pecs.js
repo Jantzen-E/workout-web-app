d74114f851110a3efa7ff22680ae3d91ffe041ef
Jantzen-E <jantzen.egan@gmail.com>
2020-12-16 20:38:05 -0700

    add exercise instructions to pecs and tri components

--- src/components/Pecs.js
+++ src/components/Pecs.js
@@ -30,7 +30,9 @@ function Pecs() {
                 <tr>
                     <td><img src={ pecsImage } className="pecsImage" alt="pectoralis muscles"></img></td>
                     <td className="nameOfExercise">incline bench press</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Perform a bench press with a slight incline to work out the upper 
+                        portion of the chest as shown in the video.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ inclineBenchThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -41,7 +43,9 @@ function Pecs() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">flat bench press</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Press a barbell straight up from a flat bench to work
+                        the mid chest as shown in the video.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ benchPressThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -52,7 +56,9 @@ function Pecs() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">decline bench press</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Put the bench at a slight decline and press the barbell
+                        upwards to work out the lower portion of the chest as shown in the video.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ declineBenchThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -63,7 +69,9 @@ function Pecs() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">chest dumbell press</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Perform a bench press with dumbbells instead of a barbell which allows
+                        your hands to get closer together at the top of the exercise.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ dumbbellPressThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -74,7 +82,9 @@ function Pecs() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">chest flys/cable crossovers (3 directions)</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">You can use bands, cables, or weights to pull at 3 different angles
+                        as shown in the video.  This will work all areas of the pec muscles.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ flyCrossoverThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -85,7 +95,9 @@ function Pecs() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">mountain climber pushups</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Perform a push up while one leg is bent with your knee close to your
+                        elbow.  Push up then switch legs and repeat.  See video for more details.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ mountainClimberThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -96,7 +108,7 @@ function Pecs() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">standard pushups</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Do a standard width push up and repeat until failure</td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ standardPushupThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -107,7 +119,9 @@ function Pecs() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">military pushups</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Do a standard push up but with your hands slightly closer together so
+                        that they are directly under your shoulders.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ militaryPushThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -118,7 +132,7 @@ function Pecs() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">wide pushups</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Do a push up but with your hands placed wider than your shoulders</td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ widePushThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -129,7 +143,9 @@ function Pecs() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">decline pushups</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Put your feet up on a chair or something higher than where your hands
+                        are so that you can work your upper chest as you do push ups until failure
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ declinePushThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -140,7 +156,7 @@ function Pecs() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">dive bomber pushups</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">See video for details on how to perform this exercise</td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ divePushThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -151,7 +167,9 @@ function Pecs() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">staggered hands pushups</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Do a push up with your right hand slightly back then switch hands after
+                        every push up
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ staggeredThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -162,7 +180,10 @@ function Pecs() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">prison cell pushups</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Do a push up then at the top of the exercise, bring your knee forward.  
+                        Then extend your leg back to normal push up position.  Do another push up and repeat with the
+                        opposite leg.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ prisonCellThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -173,7 +194,10 @@ function Pecs() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">slo-mo pushups</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Lower your body very slowly as you perform a push up then raise your 
+                        body at a normal pace.  Then lower your body at a normal pace but push up very slowly.  Then
+                        do both lowering and pushing up very slowly.  Repeat until failure.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ sloMoThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -184,7 +208,10 @@ function Pecs() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">side to side pushups</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">From the standard push up position, step one of your hands out wider
+                        to the side and perform a push up.  Then go back to standard push up position.  Then step your
+                        opposite hand out to the side and perform another push up.  Repeat until failure.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ sidePushThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
--- src/components/Triceps.js
+++ src/components/Triceps.js
@@ -28,7 +28,9 @@ function Triceps() {
                 <tr>
                     <td><img src={ tricepsImage } className="tricepsImage" alt="triceps muscle"></img></td>
                     <td className="nameOfExercise">Tricep pushups</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Perform a push up with your hands closer together to put the focus on 
+                        the triceps.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ triPushupThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -39,7 +41,10 @@ function Triceps() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">side tri rise</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Lay down on one side and grab your upper shoulder with the hand that is 
+                        closest to the ground.  Plant your hand next to your side and push the weight of your body upward
+                        to target one tricep at a time.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ sideTriRiseThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -50,7 +55,9 @@ function Triceps() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">diamond pushups</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Perform a push up similar to the tricep push up but turn your hands
+                        inward so that they make a diamond shape
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ diamondPushupThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -61,7 +68,10 @@ function Triceps() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">laying down skull crushers</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Hold dumbbells in each hand and lay down on your back on a bench
+                        or on the floor.  Extend the weights above your head but angled slightly back and then bend
+                        at the elbow and lower the weights down next to your ears.  Then raise them up again.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ skullCrusherThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -72,7 +82,11 @@ function Triceps() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">chair dips</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Sit on the edge of a chair or bench and set your hands next to your
+                        glutes.  Lift the weight of your body off the chair by extending your triceps and then lower
+                        your body down just in front of the chair with your back sliding close to the surface of the
+                        chair.  Lift your weight up until the tricep fully extends and repeat.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ chairDipThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -83,7 +97,10 @@ function Triceps() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">dumbell tricep press</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Lay down on a bench and hold 2 dumbbells close together on your
+                        chest.  Raise them up together as they are touching and squeeze them together as you raise them.
+                        Lower them back down to your chest and repeat.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ tricepPressThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -94,7 +111,10 @@ function Triceps() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">sphinx pushup</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Lay on your stomach with your weight on your elbows and your hands
+                        pointed forward.  Lift the weight of your body upward so that your elbow is no longer touching
+                        which will strenghten the tricep.  Lower your body and repeat.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ sphinxPushupThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -105,7 +125,10 @@ function Triceps() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">chair skull crushers</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">You can kneel or stand next to a bench or chair and grab the edge
+                        with your fingertips shoulder width apart.  Bend your arms and put your head between your arms
+                        then extend your arms using your triceps which will lift your body up.  Repeat exercise.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ chairSkullCrusherThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -116,7 +139,9 @@ function Triceps() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">behind the head tricep extensions</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Hold a barbell or 2 dumbbells above your head with arms extended and 
+                        slowly lower the weight behind your head by bending the elbows.  Lift weight up again and repeat.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ triExtensionThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -127,7 +152,9 @@ function Triceps() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">3 angle tricep kickbacks</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Lean forward with dumbbells in each hand and extend the weights behind
+                        your back slowly then lower them.  Follow the 3 different grips that are shown in the video.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ tricepKickbackThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -138,7 +165,10 @@ function Triceps() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">1 hand clavicle/shoulder extension</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Perform this exercise one hand at a time.  It is similar to the behind 
+                        the head tricep extension but on the 2nd repetition, lower the arm in front of your head and to
+                        touch your clavicle.  Refer to the video for the correct form.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ clavShExtensionThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -149,7 +179,10 @@ function Triceps() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">iso tri extension burnout</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Perform the beginning portion of the tricep kickback but instead of 
+                        lowering it, keep it extended behind you for an isometric tricep burnout.  Keep your arms there
+                        for as long as you can.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ isoBurnoutThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -160,7 +193,9 @@ function Triceps() {
                 <tr>
                     <td></td>
                     <td className="nameOfExercise">cobra pushup</td>
-                    <td className="explanation">explanation here</td>
+                    <td className="explanation">Perform the exercise as shown in the video.  At the top of the exercise,
+                        your body will resemble the shape of a cobra.
+                    </td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ cobraThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
@@ -168,17 +203,6 @@ function Triceps() {
                         </video>
                     </td>
                 </tr>
-                {/* <tr>
-                    <td></td>
-                    <td className="nameOfExercise">laying down tri lifts</td>
-                    <td className="explanation">explanation here</td>
-                    <td>
-                        <video width="320" height="240" autoplay controls muted poster={ supinationThumbnail }>
-                            <source src={ trapVideo } type="video/mp4"/>
-                            Your browser does not support the video tag
-                        </video>
-                    </td>
-                </tr> */}
             </table>
         </div>
     )
