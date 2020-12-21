b1449f60b14af7b7fd1502f226eabb901e13afd1
Jantzen-E <jantzen.egan@gmail.com>
2020-12-20 23:47:41 -0700

    add exercises for glutes and hamstring components

--- src/components/Glutes.js
+++ src/components/Glutes.js
@@ -2,7 +2,9 @@ import React from 'react';
 import glutesImage from '../images/glutes/glutes.png';
 import glutesImage2 from '../images/glutes/glutes2.jpg';
 import trapVideo from '../videos/traps/video.mp4';
-import pullUpThumbnail from '../images/lats/pullUpThumbnail.jpg';
+import squatThumbnail from '../images/glutes/squatThumbnail.png';
+import hipAbductionThumbnail from '../images/glutes/hipAbductionThumbnail.jpg';
+import thrustThumbnail from '../images/glutes/thrustThumbnail.jpg';
 
 function Glutes() {
     return(
@@ -16,10 +18,39 @@ function Glutes() {
                 </tr>
                 <tr>
                     <td><img src={ glutesImage } className="glutesImage" alt="glute muscles"></img><img src={ glutesImage2 } className="glutesImage2" alt="gluteus medius muscles"></img></td>
-                    <td className="nameOfExercise">Weighted arm raises</td>
-                    <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                    <td className="nameOfExercise">Squats</td>
+                    <td className="explanation">Bend legs so that your body goes into a squat position.  Go as 
+                        deep as you can putting most of your weight on your heels, then push your body back up with 
+                        your legs.
+                    </td>
                     <td>
-                        <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                        <video width="320" height="240" autoplay controls muted poster={ squatThumbnail }>
+                            <source src={ trapVideo } type="video/mp4"/>
+                            Your browser does not support the video tag
+                        </video>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td className="nameOfExercise">Hip Abduction</td>
+                    <td className="explanation">Swing your legs outward against weight that is pushing inward to 
+                        build your glutes.  See video for more details.
+                    </td>
+                    <td>
+                        <video width="320" height="240" autoplay controls muted poster={ hipAbductionThumbnail }>
+                            <source src={ trapVideo } type="video/mp4"/>
+                            Your browser does not support the video tag
+                        </video>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td className="nameOfExercise">Hip thrusts</td>
+                    <td className="explanation">Push your hips up toward the ceiling when there is weight pressing
+                        down for resistance.  Repeat until failure.
+                    </td>
+                    <td>
+                        <video width="320" height="240" autoplay controls muted poster={ thrustThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
                             Your browser does not support the video tag
                         </video>
--- src/components/Hamstrings.js
+++ src/components/Hamstrings.js
@@ -15,7 +15,51 @@ function Hamstrings() {
                 </tr>
                 <tr>
                     <td><img src={ hamstringsImage } className="hamstringsImage" alt="hamstring muscles"></img></td>
-                    <td className="nameOfExercise">Weighted arm raises</td>
+                    <td className="nameOfExercise">Hamstring curl</td>
+                    <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                    <td>
+                        <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                            <source src={ trapVideo } type="video/mp4"/>
+                            Your browser does not support the video tag
+                        </video>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td className="nameOfExercise">bridge on rolling chair or exercise ball</td>
+                    <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                    <td>
+                        <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                            <source src={ trapVideo } type="video/mp4"/>
+                            Your browser does not support the video tag
+                        </video>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td className="nameOfExercise">Hamstring curl with ankles anchored</td>
+                    <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                    <td>
+                        <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                            <source src={ trapVideo } type="video/mp4"/>
+                            Your browser does not support the video tag
+                        </video>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td className="nameOfExercise">Stiff-legged deadlift</td>
+                    <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                    <td>
+                        <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                            <source src={ trapVideo } type="video/mp4"/>
+                            Your browser does not support the video tag
+                        </video>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td className="nameOfExercise">Lunges</td>
                     <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
                     <td>
                         <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
