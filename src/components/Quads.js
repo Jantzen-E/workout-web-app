4c03c512282e9757903a360332d71d3796cd4657
Jantzen-E <jantzen.egan@gmail.com>
2020-12-21 23:58:37 -0700

    add thumbnails to quad component

--- src/components/Quads.js
+++ src/components/Quads.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import quadsImage from '../images/quads/quads.png';
 import trapVideo from '../videos/traps/video.mp4';
-import pullUpThumbnail from '../images/lats/pullUpThumbnail.jpg';
+import pistolSquatThumbnail from '../images/quads/pistolSquatThumbnail.jpg';
+import wallSitThumbnail from '../images/quads/wallSitThumbnail.jpg';
+import legPressThumbnail from '../images/quads/legPressThumbnail.jpg';
+import extensionThumbnail from '../images/quads/extensionThumbnail.jpg';
+import bulgarianThumbnail from '../images/quads/bulgarianThumbnail.jpg';
 
 function Quads() {
     return(
@@ -15,10 +19,65 @@ function Quads() {
                 </tr>
                 <tr>
                     <td><img src={ quadsImage } className="quadsImage" alt="quadriceps muscles"></img></td>
-                    <td className="nameOfExercise">Weighted arm raises</td>
-                    <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                    <td className="nameOfExercise">Assisted pistol squats</td>
+                    <td className="explanation">Do a squat with one leg and at the bottom of the movement, you sit on
+                        a bench or chair then push your body weight back up with that same leg again.  Repeat until
+                        failure then switch legs.
+                    </td>
                     <td>
-                        <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                        <video width="320" height="240" autoplay controls muted poster={ pistolSquatThumbnail }>
+                            <source src={ trapVideo } type="video/mp4"/>
+                            Your browser does not support the video tag
+                        </video>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td className="nameOfExercise">Wall sit pulses</td>
+                    <td className="explanation">Do a wall sit but instead of holding still, pulse up and down so that
+                        your 
+                    </td>
+                    <td>
+                        <video width="320" height="240" autoplay controls muted poster={ wallSitThumbnail }>
+                            <source src={ trapVideo } type="video/mp4"/>
+                            Your browser does not support the video tag
+                        </video>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td className="nameOfExercise">Leg press</td>
+                    <td className="explanation">Do a wall sit but instead of holding still, pulse up and down so that
+                        your 
+                    </td>
+                    <td>
+                        <video width="320" height="240" autoplay controls muted poster={ legPressThumbnail }>
+                            <source src={ trapVideo } type="video/mp4"/>
+                            Your browser does not support the video tag
+                        </video>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td className="nameOfExercise">Leg extensions</td>
+                    <td className="explanation">Do a wall sit but instead of holding still, pulse up and down so that
+                        your 
+                    </td>
+                    <td>
+                        <video width="320" height="240" autoplay controls muted poster={ extensionThumbnail }>
+                            <source src={ trapVideo } type="video/mp4"/>
+                            Your browser does not support the video tag
+                        </video>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td className="nameOfExercise">Bulgarian split squats</td>
+                    <td className="explanation">Do a wall sit but instead of holding still, pulse up and down so that
+                        your 
+                    </td>
+                    <td>
+                        <video width="320" height="240" autoplay controls muted poster={ bulgarianThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
                             Your browser does not support the video tag
                         </video>
