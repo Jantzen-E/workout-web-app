04a91696506be1eccabcee3958577d0127548243
Jantzen-E <jantzen.egan@gmail.com>
2020-12-22 00:23:55 -0700

    added thumbnail images to hamstring component

--- src/components/Hamstrings.js
+++ src/components/Hamstrings.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import hamstringsImage from '../images/hamstrings/hamstrings.jpg';
 import trapVideo from '../videos/traps/video.mp4';
-import pullUpThumbnail from '../images/lats/pullUpThumbnail.jpg';
+import curlThumbnail from '../images/hamstrings/curlThumbnail.jpg';
+import exerciseBallThumbnail from '../images/hamstrings/exerciseBallThumbnail.jpg';
+import nordicCurlThumbnail from '../images/hamstrings/nordicCurlThumbnail.jpg';
+import stiffLegThumbnail from '../images/hamstrings/stiffLegThumbnail.jpg';
+import lungeThumbnail from '../images/hamstrings/lungeThumbnail.jpg';
+import sideLungeThumbnail from '../images/hamstrings/sideLungeThumbnail.jpg';
+import stepOutThumbnail from '../images/hamstrings/stepOutThumbnail.png';
 
 function Hamstrings() {
     return(
@@ -18,7 +24,7 @@ function Hamstrings() {
                     <td className="nameOfExercise">Hamstring curl</td>
                     <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
                     <td>
-                        <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                        <video width="320" height="240" autoplay controls muted poster={ curlThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
                             Your browser does not support the video tag
                         </video>
@@ -29,7 +35,7 @@ function Hamstrings() {
                     <td className="nameOfExercise">bridge on rolling chair or exercise ball</td>
                     <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
                     <td>
-                        <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                        <video width="320" height="240" autoplay controls muted poster={ exerciseBallThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
                             Your browser does not support the video tag
                         </video>
@@ -37,10 +43,10 @@ function Hamstrings() {
                 </tr>
                 <tr>
                     <td></td>
-                    <td className="nameOfExercise">Hamstring curl with ankles anchored</td>
+                    <td className="nameOfExercise">Nordic Hamstring Curl</td>
                     <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
                     <td>
-                        <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                        <video width="320" height="240" autoplay controls muted poster={ nordicCurlThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
                             Your browser does not support the video tag
                         </video>
@@ -48,10 +54,10 @@ function Hamstrings() {
                 </tr>
                 <tr>
                     <td></td>
-                    <td className="nameOfExercise">Stiff-legged deadlift</td>
+                    <td className="nameOfExercise">Stiff-leg deadlift</td>
                     <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
                     <td>
-                        <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                        <video width="320" height="240" autoplay controls muted poster={ stiffLegThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
                             Your browser does not support the video tag
                         </video>
@@ -62,7 +68,29 @@ function Hamstrings() {
                     <td className="nameOfExercise">Lunges</td>
                     <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
                     <td>
-                        <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                        <video width="320" height="240" autoplay controls muted poster={ lungeThumbnail }>
+                            <source src={ trapVideo } type="video/mp4"/>
+                            Your browser does not support the video tag
+                        </video>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td className="nameOfExercise">side lunges</td>
+                    <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                    <td>
+                        <video width="320" height="240" autoplay controls muted poster={ sideLungeThumbnail }>
+                            <source src={ trapVideo } type="video/mp4"/>
+                            Your browser does not support the video tag
+                        </video>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td className="nameOfExercise">banded step-outs</td>
+                    <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                    <td>
+                        <video width="320" height="240" autoplay controls muted poster={ stepOutThumbnail }>
                             <source src={ trapVideo } type="video/mp4"/>
                             Your browser does not support the video tag
                         </video>
