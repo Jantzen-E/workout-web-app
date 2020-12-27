a33b92e88024cc64a9ce628fbf86cedca68e52d0
Jantzen-E <jantzen.egan@gmail.com>
2020-12-26 23:57:08 -0700

    add exercises to abs component

--- src/components/Abs.js
+++ src/components/Abs.js
@@ -18,7 +18,194 @@ function Abs() {
                     </tr>
                     <tr>
                         <td><img src={ absImage } className="absImage" alt="ab muscles"></img><img src={ obliquesImage } className="obliquesImage" alt="oblique ab muscles"></img><img src={ serratusImage } className="serratusImage" alt="serratus anterior muscles"></img></td>
-                        <td className="nameOfExercise">Weighted arm raises</td>
+                        <td className="nameOfExercise">Bicycles</td>
+                        <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                        <td>
+                            <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                                <source src={ trapVideo } type="video/mp4"/>
+                                Your browser does not support the video tag
+                            </video>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className="nameOfExercise">Heels to the heavens</td>
+                        <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                        <td>
+                            <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                                <source src={ trapVideo } type="video/mp4"/>
+                                Your browser does not support the video tag
+                            </video>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className="nameOfExercise">Crunches</td>
+                        <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                        <td>
+                            <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                                <source src={ trapVideo } type="video/mp4"/>
+                                Your browser does not support the video tag
+                            </video>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className="nameOfExercise">Reverse Crunches</td>
+                        <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                        <td>
+                            <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                                <source src={ trapVideo } type="video/mp4"/>
+                                Your browser does not support the video tag
+                            </video>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className="nameOfExercise">Scissors</td>
+                        <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                        <td>
+                            <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                                <source src={ trapVideo } type="video/mp4"/>
+                                Your browser does not support the video tag
+                            </video>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className="nameOfExercise">Flutter kick</td>
+                        <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                        <td>
+                            <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                                <source src={ trapVideo } type="video/mp4"/>
+                                Your browser does not support the video tag
+                            </video>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className="nameOfExercise">Mason Twist</td>
+                        <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                        <td>
+                            <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                                <source src={ trapVideo } type="video/mp4"/>
+                                Your browser does not support the video tag
+                            </video>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className="nameOfExercise">Side Crunches</td>
+                        <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                        <td>
+                            <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                                <source src={ trapVideo } type="video/mp4"/>
+                                Your browser does not support the video tag
+                            </video>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className="nameOfExercise">Circle Crunches</td>
+                        <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                        <td>
+                            <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                                <source src={ trapVideo } type="video/mp4"/>
+                                Your browser does not support the video tag
+                            </video>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className="nameOfExercise">Sit ups</td>
+                        <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                        <td>
+                            <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                                <source src={ trapVideo } type="video/mp4"/>
+                                Your browser does not support the video tag
+                            </video>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className="nameOfExercise">heel touches</td>
+                        <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                        <td>
+                            <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                                <source src={ trapVideo } type="video/mp4"/>
+                                Your browser does not support the video tag
+                            </video>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className="nameOfExercise">knee crunches</td>
+                        <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                        <td>
+                            <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                                <source src={ trapVideo } type="video/mp4"/>
+                                Your browser does not support the video tag
+                            </video>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className="nameOfExercise">side hip raise</td>
+                        <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                        <td>
+                            <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                                <source src={ trapVideo } type="video/mp4"/>
+                                Your browser does not support the video tag
+                            </video>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className="nameOfExercise">in and outs</td>
+                        <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                        <td>
+                            <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                                <source src={ trapVideo } type="video/mp4"/>
+                                Your browser does not support the video tag
+                            </video>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className="nameOfExercise">Hang from pullup bar leg raise</td>
+                        <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                        <td>
+                            <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                                <source src={ trapVideo } type="video/mp4"/>
+                                Your browser does not support the video tag
+                            </video>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className="nameOfExercise">cross leg/wide leg sit up</td>
+                        <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                        <td>
+                            <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                                <source src={ trapVideo } type="video/mp4"/>
+                                Your browser does not support the video tag
+                            </video>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className="nameOfExercise">leg climb</td>
+                        <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
+                        <td>
+                            <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
+                                <source src={ trapVideo } type="video/mp4"/>
+                                Your browser does not support the video tag
+                            </video>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className="nameOfExercise">plank variations</td>
                         <td className="explanation">Hold weight in each hand and shrug your shoulders</td>
                         <td>
                             <video width="320" height="240" autoplay controls muted poster={ pullUpThumbnail }>
