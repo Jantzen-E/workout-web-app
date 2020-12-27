d213aa573ab510b1b9fa9f629332aaf2710b137c
Jantzen-E <jantzen.egan@gmail.com>
2020-12-27 15:06:09 -0700

    add content to the home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,47 @@
 import React from 'react';
-// import Traps from './Traps.js';
-// import Delts from './Delts.js';
-// import Lats from './Lats.js';
-// import Biceps from './Biceps.js';
-// import Triceps from './Triceps.js';
-// import Pecs from './Pecs.js';
-// import Abs from './Abs.js';
-// import Forearms from './Forearms.js';
-// import Glutes from './Glutes.js';
-// import Hamstrings from './Hamstrings.js';
-// import Quads from './Quads.js';
-// import Calves from './Calves.js';
-// import Adductors from './Adductors.js';
-// import Abductors from './Abductors.js';
-// import Cardio from './Cardio.js';
-
+ 
 function Home() {
     return(
-        <div>
-            {/* <Traps />
-            <Delts />
-            <Lats />
-            <Biceps />
-            <Triceps />
-            <Pecs />
-            <Abs />
-            <Forearms />
-            <Glutes />
-            <Hamstrings />
-            <Adductors />
-            <Abductors />
-            <Quads />
-            <Calves />
-            <Cardio /> */}
+        <div className="homePage">
+            <p>
+                <span className="questions">WHAT IS THIS WEBSITE?:</span> This website is a compilation of exercises that I have learned over the years from people who are a lot
+                smarter than I am.  I have learned these from well-known workout programs, fitness gurus/body builders on social media,
+                and from trying lots of different exercises out to find which ones I feel like work better for me personally. 
+            </p>
+            <hr/>
+            <p>
+                <span className="questions">WHO IS THIS SITE FOR?:</span> This is for anyone who wants to improve their life by doing exercise.  Exercise helps you feel better
+                by releasing chemicals in your brain that can improve your mood.  It can help improve depression, winter blues, and feelings of laziness and lack of energy.
+                This site is for people who want to see which exercise works which muscle so that they can improve their physique which can boost self esteem.
+            </p>
+            <hr/>
+            <p>
+                <span className="questions">WHY USE THIS SITE?:</span>  Use this site if you want to know which exercise works out certain muscles/muscle groups.  I broke it down in 
+                a simple way so that you can find exercises that focus primarily on specific muscle groups so that you can tailor your workouts to your needs.  For example,
+                if you feel like your upper body is in better shape than your legs, you can put a greater focus on the glutes, hamstrings, quads, calves etc. to build those
+                muscles so that you are satisfied with your overall look and so you are well balanced with your fitness goals.
+            </p>
+            <hr/>
+            <p>
+                <span className="questions">WHEN DO I USE THIS SITE?:</span>
+            </p>
+            <hr/>
+            <p>
+                <span className="questions">HOW OFTEN DO I EXERCISE EACH MUSCLE GROUP?:</span>
+            </p>
+            <hr/>
+            <p>
+                <span className="questions">WHICH EXERCISES SHOULD I DO?:</span>  This all depends on what your goals are.  In my opinion, I would put a stronger focus on the
+                areas where you would like to improve with a lesser focus on the other areas.  I would still recommend working out each muscle group but just change the focus
+                based on your personal goals.
+            </p>
+            <hr/>
+            <p>
+                <span className="questions">WHERE CAN I DO THESE EXERCISES?:</span>  Many of these exercises can be done from home with minimal equipment.  If you have 
+                exercise bands or dumbbells then you can do most of these.  Other equipment that is used are things that are common household items like chairs, couches,
+                doors, etc.  If you find an exercise that requires gym equipment that you don't have access to, there are alternative exercises that will most likely work so 
+                you can do your research if you don't find all the alternatives in this site.
+            </p>
         </div>
     )
 }
