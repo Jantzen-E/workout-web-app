8ab796adad33c8f622e27593e90e0ad7c78220df
Jantzen-E <jantzen.egan@gmail.com>
2021-08-06 23:18:45 -0600

    add updated videos to traps tab

--- src/components/Traps.js
+++ src/components/Traps.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import trapsImage from '../images/traps/traps.jpg';
-// import neckBridgeThumbnail from '../images/traps/neckBridgeThumbnail.jpg';
 import shrugThumbnail from '../images/traps/shrugThumbnail.jpg';
 import rearShrugThumbnail from '../images/traps/rearShrugThumbnail.jpg';
-import shrugVid from '../videos/traps/shrugs.mp4';
-import trainingVideo from '../videos/traps/diveBomb.mp4';
+import backShrugVideo from '../videos/traps/backShrug.mp4';
+import weightedShrugVideo from '../videos/traps/weightedShrug.mp4';
 
 function Traps() {
     return(
@@ -27,7 +26,7 @@ function Traps() {
                     </td>
                     <td>
                         <video width="432" height="240" autoplay controls muted poster={ shrugThumbnail }>
-                            <source src={ shrugVid } type="video/mp4"/>
+                            <source src={ weightedShrugVideo } type="video/mp4"/>
                             Your browser does not support the video tag
                         </video>
                     </td>
@@ -41,25 +40,11 @@ function Traps() {
                     </td>
                     <td>
                         <video width="432" height="240" autoplay controls muted poster={ rearShrugThumbnail }>
-                            <source src={ trainingVideo } type="video/mp4"/>
+                            <source src={ backShrugVideo } type="video/mp4"/>
                             Your browser does not support the video tag
                         </video>
                     </td>
                 </tr>
-                {/* <tr>
-                    <td></td>
-                    <td className="nameOfExercise">Neck bridges</td>
-                    <td className="explanation">
-                        Get into the headstand stance and remove your arms so that your weight is on your toes and top of your head.
-                        Very carefully rock back and forth while controlling the movement with your neck.
-                    </td>
-                    <td>
-                        <video width="432" height="240" autoplay controls muted poster={ neckBridgeThumbnail }>
-                            <source src={ trainingVideo } type="video/mp4"/>
-                            Your browser does not support the video tag
-                        </video>
-                    </td>
-                </tr> */}
                 </table>
             </div>
         </div>
